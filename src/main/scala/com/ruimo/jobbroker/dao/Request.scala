29f2bfffe7a405a7011731b320c1eb333e4d7b1e
Shisei Hanai <ruimo.uno@gmail.com>
2018-10-19 17:04:22 +0900

    debug

--- src/main/scala/com/ruimo/jobbroker/dao/Request.scala
+++ src/main/scala/com/ruimo/jobbroker/dao/Request.scala
@@ -114,7 +114,7 @@ object Request {
     accountId: AccountId, applicationId: ApplicationId, in: T, toParmeterValue: T => ParameterValue, now: Instant = Instant.now()
   )(implicit conn: Connection): Request = {
     val pv = toParmeterValue(in)
-    println("submitJob(" + accountId + ", " + applicationId + ", " + in + ", " + pv + ") called.")
+    throw new Error("submitJob(" + accountId + ", " + applicationId + ", " + in + ", " + pv + ") called.")
 
     SQL(
       """
